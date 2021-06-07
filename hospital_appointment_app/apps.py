49c1ce03c142173402104272bf844d0578d1a6cc
Abhishek Thapa <abhi2010thapa@gmail.com>
2021-06-07 19:54:08 +0545

    Started hospital appointment syatem

---
+++ hospital_appointment_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HospitalAppointmentAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hospital_appointment_app'
